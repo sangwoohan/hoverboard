0cdfac9f2cb528a72793bd2e951aa4b03b975572
razic <razic@viralkitty.com>
2012-12-26 13:29:02 -0800

    add socket server and touch handlers

--- hoverboard.js
+++ hoverboard.js
@@ -1,8 +1,12 @@
+var port;
 var httpServer;
 var fileServer;
+var socketServer;
 
+port = 8080;
 httpServer = require('./lib/http-server');
 fileServer = require('./lib/file-server');
+socketServer = require('./lib/socket-server');
 
 fileServer.setRoot('./public');
-httpServer.listen(fileServer.listener, 8080);
+socketServer.listen(httpServer.listen(fileServer.listener, port));
--- public/js/hoverboard.js
+++ public/js/hoverboard.js
@@ -1,1 +1,23 @@
-console.log('foo');
+var socket;
+
+socket = new WebSocket("ws://" + location.host);
+
+function touchStart (event) {
+  event.preventDefault();
+}
+
+function touchMove (event) {
+  event.preventDefault();
+}
+
+function touchEnd (event) {
+  event.preventDefault();
+}
+
+function socketOpen () {
+  document.addEventListener("touchstart", touchStart, false);
+  document.addEventListener("touchmove", touchMove, false);
+  document.addEventListener("touchend", touchEnd, false);
+}
+
+socket.onopen = socketOpen;
