54dfe4676b7551626b23af89b88197ace7f4af7c
razic <razic@viralkitty.com>
2012-12-26 01:05:30 -0800

    set up static file server

--- hoverboard.js
+++ hoverboard.js
@@ -1,1 +1,8 @@
-require('./lib/server');
+var httpServer;
+var fileServer;
+
+httpServer = require('./lib/http-server');
+fileServer = require('./lib/file-server');
+
+fileServer.setDir('./public');
+httpServer.listen(8080, fileServer.listener);
