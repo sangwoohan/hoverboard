969a2b85f1acaaaef2a1dfd1d127ff5cabd5a3bb
razic <razic@viralkitty.com>
2012-12-27 22:27:14 -0800

    remove touch.js for now and update interfacing with touch

--- public/js/hoverboard.js
+++ public/js/hoverboard.js
@@ -3,7 +3,20 @@ var socket;
 socket = new WebSocket("ws://" + location.host);
 
 function socketOpen() {
-  new TapRecognizer(document);
+  var singleClickRecognizer;
+
+  singleClickRecognizer = new TapRecognizer(document);
+
+  function singleClickStart(recognizer) {
+    console.log('click started');
+  }
+
+  function singleClickEnd(recognizer) {
+    console.log('click ended');
+  }
+
+  singleClickRecognizer.onStart = singleClickStart;
+  singleClickRecognizer.onEnd = singleClickEnd;
 }
 
 socket.onopen = socketOpen;
